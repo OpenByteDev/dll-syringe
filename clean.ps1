8413fd706f4d57b8214039aecd0df086c43801d7
OpenByte <development.openbyte@gmail.com>
2022-01-28 18:36:52 +0100

    Add conveniece test and clean ps script

---
+++ clean.ps1
@@ -0,0 +1,9 @@
+cargo clean
+
+Set-Location .\tests\helpers\test_payload
+cargo clean
+Set-Location ..\..\..
+
+Set-Location .\tests\helpers\test_target
+cargo clean
+Set-Location ..\..\..
---
+++ test.ps1
@@ -0,0 +1,2 @@
+cargo test --target i686-pc-windows-msvc -- --nocapture
+cargo test --target x86_64-pc-windows-msvc -- --nocapture
