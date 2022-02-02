1e55d723e3f5df4e0cdd154fc0966874f9f6ea46
OpenByte <development.openbyte@gmail.com>
2022-02-02 03:39:43 +0100

    Use thiserror instead of quick-error

--- src/error.rs
+++ src/error.rs
@@ -1,143 +1,89 @@
 use std::io;
 
-use quick_error::quick_error;
+use thiserror::Error;
 
-quick_error! {
-    #[derive(Debug)]
-    /// Error enum representing either a win32 api error or a nul error from an invalid interior nul.
-    pub enum Win32OrNulError {
-        /// Variant representing an illegal interior nul value.
-        Nul(source: widestring::NulError<u16>) {
-            from()
-            display("Invalid nul value in argument: {}", source)
-            source(source)
-        }
-        /// Variant representing an windows api error.
-        Win32(source: rust_win32error::Win32Error) {
-            from()
-            display("Windows API call failed: {}", source)
-            source(source)
-        }
-    }
+#[derive(Debug, Error)]
+/// Error enum representing either a win32 api error or a nul error from an invalid interior nul.
+pub enum Win32OrNulError {
+    /// Variant representing an illegal interior nul value.
+    #[error("interior nul found")]
+    Nul(#[from] widestring::NulError<u16>),
+    /// Variant representing an windows api error.
+    #[error("windows api error: {}", _0)]
+    Win32(#[from] rust_win32error::Win32Error),
 }
 
-quick_error! {
-    /// Error enum for errors during a call to [`ProcessModule::get_procedure`].
-    ///
-    /// [`ProcessModule::get_procedure`]: ProcessModule::get_procedure
-    #[derive(Debug)]
-    pub enum ProcedureLoadError {
-        /// Variant representing an illegal interior nul value.
-        Nul(source: std::ffi::NulError) {
-            from()
-            display("Invalid nul value in argument: {}", source)
-            source(source)
-        }
-        /// Variant representing an windows api error.
-        Win32(source: rust_win32error::Win32Error) {
-            from()
-            display("Windows API call failed: {}", source)
-            source(source)
-        }
-        /// Variant representing an unsupported target process.
-        UnsupportedTarget {
-            display("The requested operation is not supported for the target process.")
-        }
-    }
+/// Error enum for errors during a call to [`ProcessModule::get_procedure`].
+///
+/// [`ProcessModule::get_procedure`]: ProcessModule::get_procedure
+#[derive(Debug, Error)]
+pub enum ProcedureLoadError {
+    /// Variant representing an illegal interior nul value.
+    #[error("interior nul found")]
+    Nul(#[from] std::ffi::NulError),
+    /// Variant representing an windows api error.
+    #[error("windows api error: {}", _0)]
+    Win32(#[from] rust_win32error::Win32Error),
+    /// Variant representing an unsupported target process.
+    #[error("unsupported target process")]
+    UnsupportedTarget,
 }
 
-quick_error! {
-    /// Error enum for errors during a call to [`ProcessModule::from_path`] and related methods.
-    ///
-    /// [`ProcessModule::from_path`]: ProcessModule::from_path
-    #[derive(Debug)]
-    pub enum ModuleFromPathError {
-        /// Variant representing an illegal interior nul value.
-        Nul(source: widestring::NulError<u16>) {
-            from()
-            display("Invalid nul value in argument: {}", source)
-            source(source)
-        }
-        /// Variant representing an windows api error.
-        Win32(source: rust_win32error::Win32Error) {
-            from()
-            display("Windows API call failed: {}", source)
-            source(source)
-        }
-        /// Variant representing a general io error.
-        Io(source: io::Error) {
-            from()
-            display("Io error: {}", source)
-            source(source)
-        }
-    }
+/// Error enum for errors during a call to [`ProcessModule::from_path`] and related methods.
+///
+/// [`ProcessModule::from_path`]: ProcessModule::from_path
+#[derive(Debug, Error)]
+pub enum ModuleFromPathError {
+    /// Variant representing an illegal interior nul value.
+    #[error("interior nul found")]
+    Nul(#[from] widestring::NulError<u16>),
+    /// Variant representing an windows api error.
+    #[error("windows api error: {}", _0)]
+    Win32(#[from] rust_win32error::Win32Error),
+    /// Variant representing a general io error.
+    #[error("io error: {}", _0)]
+    Io(#[from] io::Error),
 }
 
 impl From<Win32OrNulError> for ModuleFromPathError {
     fn from(err: Win32OrNulError) -> Self {
         match err {
-            Win32OrNulError::Nul(e) => ModuleFromPathError::Nul(e),
-            Win32OrNulError::Win32(e) => ModuleFromPathError::Win32(e),
+            Win32OrNulError::Nul(e) => Self::Nul(e),
+            Win32OrNulError::Win32(e) => Self::Win32(e),
         }
     }
 }
 
-quick_error! {
-    /// Error enum for errors during injection and ejection.
-    #[derive(Debug)]
-    pub enum InjectError {
-        /// Variant representing an illegal interior nul value.
-        Nul(source: widestring::NulError<u16>) {
-            from()
-            display("Invalid nul value in argument: {}", source)
-            source(source)
-        }
-        /// Variant representing an windows api error.
-        Win32(source: rust_win32error::Win32Error) {
-            from()
-            display("Windows API call failed: {}", source)
-            source(source)
-        }
-        /// Variant representing an unsupported target process.
-        UnsupportedTarget {
-            display("The requested operation is not supported for the target process.")
-        }
-        /// Variant representing a failed operation inside the target process.
-        RemoteOperationFailed {
-            display("An operation failed inside the remote process.")
-        }
-        /// Variant representing a general io error.
-        Io(source: io::Error) {
-            from()
-            display("Io error: {}", source)
-            source(source)
-        }
-        #[cfg(target_arch = "x86_64")]
-        #[cfg(feature = "into_x86_from_x64")]
-        /// Variant representing an error while loading an pe file.
-        Goblin(source: goblin::error::Error) {
-            display("Failed to load pe file: {}", source)
-            source(source)
-        }
-    }
-}
-
-// This cannot be done with from() like for the other error types as goblin is only present
-// on x64 with the into_x86_from_x64 feature and quick_error includes the From impl
-// unconditionally
-#[cfg(target_arch = "x86_64")]
-#[cfg(feature = "into_x86_from_x64")]
-impl From<goblin::error::Error> for InjectError {
-    fn from(source: goblin::error::Error) -> Self {
-        InjectError::Goblin(source)
-    }
+/// Error enum for errors during injection and ejection.
+#[derive(Debug, Error)]
+pub enum InjectError {
+    /// Variant representing an illegal interior nul value.
+    #[error("interior nul found")]
+    Nul(#[from] widestring::NulError<u16>),
+    /// Variant representing an windows api error.
+    #[error("windows api error: {}", _0)]
+    Win32(#[from] rust_win32error::Win32Error),
+    /// Variant representing a general io error.
+    #[error("io error: {}", _0)]
+    Io(#[from] io::Error),
+    /// Variant representing an unsupported target process.
+    #[error("unsupported target process")]
+    UnsupportedTarget,
+    /// Variant representing a failed operation inside the target process.
+    #[error("remote operation failed inside target process")]
+    RemoteOperationFailed,
+    /// Variant representing an error while loading an pe file.
+    #[cfg(target_arch = "x86_64")]
+    #[cfg(feature = "into_x86_from_x64")]
+    #[error("failed to load pe file: {}", _0)]
+    Goblin(#[from] goblin::error::Error),
 }
 
 impl From<Win32OrNulError> for InjectError {
     fn from(err: Win32OrNulError) -> Self {
         match err {
-            Win32OrNulError::Nul(e) => InjectError::Nul(e),
-            Win32OrNulError::Win32(e) => InjectError::Win32(e),
+            Win32OrNulError::Nul(e) => Self::Nul(e),
+            Win32OrNulError::Win32(e) => Self::Win32(e),
         }
     }
 }
@@ -145,9 +91,9 @@ impl From<Win32OrNulError> for InjectError {
 impl From<ModuleFromPathError> for InjectError {
     fn from(err: ModuleFromPathError) -> Self {
         match err {
-            ModuleFromPathError::Nul(e) => InjectError::Nul(e),
-            ModuleFromPathError::Win32(e) => InjectError::Win32(e),
-            ModuleFromPathError::Io(e) => InjectError::Io(e),
+            ModuleFromPathError::Nul(e) => Self::Nul(e),
+            ModuleFromPathError::Win32(e) => Self::Win32(e),
+            ModuleFromPathError::Io(e) => Self::Io(e),
         }
     }
 }
