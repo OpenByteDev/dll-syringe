66a2adce30197848db8b125bf201d8681faecea3
OpenByte <development.openbyte@gmail.com>
2022-02-06 22:05:13 +0100

    Hide process memory structs behind feature

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -7,8 +7,12 @@ pub use process_ref::*;
 mod process_module;
 pub use process_module::*;
 
+#[cfg_attr(not(feature = "process_memory"), allow(dead_code))]
 mod process_memory;
+#[cfg(feature = "process_memory")]
 pub use process_memory::*;
+#[cfg(not(feature = "process_memory"))]
+pub(crate) use process_memory::*;
 
 #[allow(dead_code)]
 mod raw_allocator;
