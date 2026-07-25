b1676d268a7ce61ab44ec784876b6054cc63fc39
OpenByte <development.openbyte@gmail.com>
2026-07-25 23:50:05 +0200

    suppress: Suppress CA2255 for intentional ModuleInitializer usage

--- dll-syringe-bindings/bindings/csharp/Init.cs
+++ dll-syringe-bindings/bindings/csharp/Init.cs
@@ -3,8 +3,10 @@ using System.Runtime.InteropServices;
 using DllSyringe.Sys;
 
 internal class Init {
+#pragma warning disable CA2255
     [ModuleInitializer]
     internal static void RegisterImportResolver() {
         NativeLibrary.SetDllImportResolver(typeof(NativeMethods).Assembly, NativeMethods.DllImportResolver);
     }
+#pragma warning restore CA2255
 }
